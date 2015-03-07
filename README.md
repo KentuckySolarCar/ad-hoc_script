cfb97b1faf7253a721e9b63a36e93ae23145b008
John Broadbent <john.broadbent@uky.edu>
2015-03-07 00:35:35 -0500

    Added the basic files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ad-hoc_script
 This script will connect to the garage wifi when it is detected. If the ssid "solarcar" can not be found the script will open a ad-hoc network
+
+UNTESTED
+DO NOT RUN THIS YET !
---
+++ S_car.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#SSID=()
+
+RESULT=$(iwlist wlan0 scan  | grep ESSID  | grep -Po '".*?"')
+
+echo $RESULT
+
+ATHOME=false
+
+for i in $RESULT; 
+   do 
+   echo $i 
+   if [ $i = '"solarcar"' ]
+   then
+        echo "This is home"
+        ATHOME=true 
+   else
+        echo "Not home"
+    fi;
+done
+
+echo "done"
+if [ $ATHOME ] 
+then
+   echo "normal mode"
+   cp defaultSetup /etc/network/interfaces
+else
+   echo "adhoc mode"
+   cp adhocSetup   /etc/network/interfaces
+
+fi
+
+ifdown wlan0
+ifup wlan0
